6f01a517bcca791e4b1c3a90d5a74febd0efcb30
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2022-08-30 01:19:26 -0400

    desafio

---
+++ script.js
@@ -0,0 +1,34 @@
+//desafio 1
+for(var i=1; i<21; i++){
+    //console.log(i);
+}
+
+//desafio 2
+for(var k=99; k>0; k--){
+    if(k%3==0){
+        //console.log(k)
+    }
+}
+
+//desafio 3
+var x = [4,2.5,1,-0.5,-2,-3.5];
+//console.log(x);
+for(var m=0; m<x.length; m++){
+    //console.log(x[m]);
+}
+
+//desafio 4
+var sum = 0;
+for(var l=1; l<101; l++){
+    sum = sum + l;
+    //console.log(sum);
+}
+
+//desafio 5
+factorial = 1;
+for(var j=1; j<13; j++){
+    factorial = j * factorial;
+    //console.log(factorial)
+}
+
+
